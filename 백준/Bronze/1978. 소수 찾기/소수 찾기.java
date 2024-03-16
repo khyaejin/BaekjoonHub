8b4ca78ecfb1af24f17448a3eb770affd012a4f8
khyaejin <147326233+khyaejin@users.noreply.github.com>
2024-03-17 04:04:16 +0900

    [Bronze II] Title: 소수 찾기, Time: 216 ms, Memory: 17724 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.java
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.java
@@ -7,10 +7,10 @@ class PrimeNumber {
 		Scanner sc = new Scanner(System.in);
 		int num = sc.nextInt();
 		int numOfPrimeNum = 0;
-		
+
 		for(int i =0; i<num; i++) {
 			number = sc.nextInt();		
-			
+
 			if(determinePrimeNum()) {
 				numOfPrimeNum++;
 			}
@@ -19,7 +19,7 @@ class PrimeNumber {
 	}
 		private boolean determinePrimeNum(){
 			if(number == 1) return false; //1은 소수아님
-			
+
 			for(int i = 2; i<number; i++) {
 				if(number % i == 0) { //1과 자신 자기 외의 수로 나누어 떨어지면 소수아님
 					return false;
