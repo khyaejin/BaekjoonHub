e781dc136ba97bf0a2ff155f6b61b944d64e1038
khyaejin <147326233+khyaejin@users.noreply.github.com>
2024-03-23 18:06:28 +0900

    [Bronze I] Title: 약수, Time: 212 ms, Memory: 17740 KB -BaekjoonHub

---
+++ 백준/Bronze/1037. 약수/약수.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+ 
+public class Main {
+ 
+	public static void main(String[] args) {
+ 
+		Scanner in = new Scanner(System.in);
+		
+		int T = in.nextInt();
+		
+		int max = Integer.MIN_VALUE;
+		int min = Integer.MAX_VALUE;
+		
+		while(T-- > 0) {
+			int N = in.nextInt();
+			if(N > max) max = N;
+			if(N < min) min = N;
+			
+		}
+		System.out.println(max * min);
+	}
+ 
+}
