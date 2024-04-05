d3e731f7852b7fed121513f826d35786cab98c1b
khyaejin <147326233+khyaejin@users.noreply.github.com>
2024-04-05 19:16:10 +0900

    [Bronze V] Title: ??!, Time: 208 ms, Memory: 17732 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+//class Chessborad{
+//	
+//	public void run() {
+//	    Scanner sc = new Scanner(System.in);
+//
+//	    sc.close();
+//	}
+//
+//}
+
+public class Main {
+    public static void main(String args[]) {
+    	Scanner sc = new Scanner(System.in);
+    	
+    	String id = sc.next();
+    	System.out.print(id+"??!");
+    	
+//    	Chessborad chessborad = new Chessborad();  	
+//    	chessborad.run();
+    	sc.close();
+    }
+}
