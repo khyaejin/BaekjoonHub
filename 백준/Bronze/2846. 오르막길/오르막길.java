b2da0c3460eb1202a5561d63e055753a47250990
khyaejin <147326233+khyaejin@users.noreply.github.com>
2024-04-07 00:20:43 +0900

    Update 오르막길.java
    수정하였음

--- 백준/Bronze/2846. 오르막길/오르막길.java
+++ 백준/Bronze/2846. 오르막길/오르막길.java
@@ -1,30 +1,37 @@
 import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int N = scan.nextInt();
-		
-		int before = 0;
-		int current = scan.nextInt();
-		int sum = 0;
-		int result = 0;
-		
-		for(int i=1; i<N; i++) {
-			before = current;
-			current = scan.nextInt();
-			if(current - before > 0) 	// 증가
-				sum += current - before;
-			else {
-				result = Math.max(result, sum);
-				sum = 0;
-			}
-		}
-		result = Math.max(result, sum);
+        int size = sc.nextInt();
 
-		System.out.println(result);
-		scan.close();
-	}
+        int[] height = new int[size];
+        int minHeight = Integer.MIN_VALUE;
+        int maxHeight = Integer.MAX_VALUE;
+        int biggestUpDownHill = 0;
+
+        height[0] = sc.nextInt(); //처음 수 입력받기
+
+        minHeight = height[0];
+
+        for (int i = 1; i < size; i++) {
+            height[i] = sc.nextInt();
+
+            if (height[i - 1] < height[i]) { //오르막길 이라면
+                maxHeight = height[i];
+            } else {//오르막길이 아니라면
+                minHeight = height[i]; //현재 높이로
+                maxHeight = 0; //초기화
+            }
+
+            if (biggestUpDownHill < maxHeight - minHeight) {//현재 오르막길의 높이가 기존 오르막길의 높이보다 크면
+                biggestUpDownHill = maxHeight - minHeight; // 가장 큰 오르막길 높이 업데이트
+            }
+
+
+        }
+        System.out.print(biggestUpDownHill);
+    }
 
 }
