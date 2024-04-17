48979648b7b245ae223868b8e2ea8e17792467c1
khyaejin <147326233+khyaejin@users.noreply.github.com>
2024-04-17 23:45:24 +0900

    [Bronze III] Title: 대회 or 인턴, Time: 204 ms, Memory: 17552 KB -BaekjoonHub

--- 백준/Bronze/2875. 대회 or 인턴/대회 or 인턴.java
+++ 백준/Bronze/2875. 대회 or 인턴/대회 or 인턴.java
@@ -11,9 +11,10 @@ public class Main {
         int count = 0;
 
         while(true){
-            if(!(girlNum>=2 && boyNum>=1 && (girlNum-2+boyNum-1)>=internShipNum)) break;
+            if(!(girlNum>=2 && boyNum>=1)) break; //팀을 결성하지 못하면 종료
             girlNum -= 2;
             boyNum -= 1;
+            if(!((girlNum+boyNum)>=internShipNum)) break; //팀 결성 후 인턴쉽 나갈 인원이 부족하면 count하지 않고 종료
             count++;
 
         }
