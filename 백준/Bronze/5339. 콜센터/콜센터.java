355d43483de0c1ae3fa7d165563baf911ad89263
khyaejin <147326233+khyaejin@users.noreply.github.com>
2024-04-20 22:43:27 +0900

    [Bronze V] Title: 콜센터, Time: 104 ms, Memory: 13740 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args)  {
+       System.out.print("     /~\\\n" +
+               "    ( oo|\n" +
+               "    _\\=/_\n" +
+               "   /  _  \\\n" +
+               "  //|/.\\|\\\\\n" +
+               " ||  \\ /  ||\n" +
+               "============\n" +
+               "|          |\n" +
+               "|          |\n" +
+               "|          |");
+    }
+}
