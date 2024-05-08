a552a184ac969bc65455d5dbf627cc4090def5cc
김혜진 <olivia019182@naver.com>
2024-05-08 23:19:22 +0900

    [Bronze V] Title: 자동완성, Time: 124 ms, Memory: 14260 KB -BaekjoonHub

--- 백준/Bronze/24883. 자동완성/자동완성.java
+++ 백준/Bronze/24883. 자동완성/자동완성.java
@@ -1,17 +1,14 @@
-import java.util.Scanner;
+import java.io.*;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    public static void main(String[] args)  {
-        Scanner sc = new Scanner(System.in);
-
-        String input = sc.next();
-
+        String input = br.readLine();
         if (input.equals("N") || input.equals("n")) {
-            System.out.print("Naver D2");
-        }
-        else{
-            System.out.print("Naver Whale");
+            System.out.println("Naver D2");
+        } else {
+            System.out.println("Naver Whale");
         }
 
     }
