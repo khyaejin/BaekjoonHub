d1d7a2f02c86f6fa0af66eac1b5142c9b7dc173d
김혜진 <olivia019182@naver.com>
2024-05-18 00:00:09 +0900

    [Bronze I] Title: 팰린드롬수, Time: 116 ms, Memory: 14076 KB -BaekjoonHub

--- 백준/Bronze/1259. 팰린드롬수/팰린드롬수.java
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.java
@@ -3,29 +3,35 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		//무한루프
-		while(true) {
-			String S = br.readLine();
-			//StringBuilder를 선언하면서 입력값인 S를 저장한다.
-			StringBuilder sb = new StringBuilder(S);
-			//StringBuilder 내장 함수 reverse를 사용하지만 원래는 같은 StringBuilder에 저장가능하기에
-			//String으로 변환해서 저장한다. 그것이 뒤에 .toString()이다.
-			String Sreverse = sb.reverse().toString();
-			
-			//0 입력시 무한루프 종료
-			if(S.equals("0")) break;
-			
-			//두 문자열을 비교
-			if(S.equals(Sreverse)) {
-				System.out.println("yes");
-			}else{
-				System.out.println("no");
-			}
-		}
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        while (true) {
+            String word = br.readLine();
+            int length = word.length();
+            int point = length - 1;
+            boolean isIt = true;
+
+            if (word.equals("0")) {
+                break;
+            }
+
+            for (int i = 0; i < length / 2; i++) {
+                if(word.charAt(i) != word.charAt(point)){
+                    isIt = false;
+                    break;
+                }
+                point--;
+            }
+            if(isIt){
+                System.out.println("yes");
+            }else {
+                System.out.println("no");
+            }
+
+
+
+        }
+
+    }
 }
