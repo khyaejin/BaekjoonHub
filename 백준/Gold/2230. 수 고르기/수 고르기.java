14e1c8dd910300377dd006d53a1f911a941b8338
김혜진 <olivia019182@naver.com>
2025-09-16 11:06:25 +0900

    Docs: [2230] 수 고르기 - 시간 복잡도 주석 추가
    2230 수 고르기(gold 5)문제의 시간 복잡도 주석을 추가해주었습니다.

--- 백준/Gold/2230. 수 고르기/수 고르기.java
+++ 백준/Gold/2230. 수 고르기/수 고르기.java
@@ -23,14 +23,14 @@ public class Main {
             arr[i] = Integer.parseInt(br.readLine());
         }
         
-        // arr 정렬
+        // arr 정렬: O(NlogN)
         Arrays.sort(arr);
 
         int L = 0, R = 0; // 조건) 가을 수 있음
         int diff = 0;
         int minDiff = Integer.MAX_VALUE;
 
-        // 두 수 차이가 M 이상, 가장 작은 경우 구하기
+        // 두 수 차이가 M 이상, 가장 작은 경우 구하기: O(N)
         while(L<N && R <N){
             diff = arr[R] - arr[L];
 
@@ -49,4 +49,5 @@ public class Main {
  * 일반적인 투포인트 방식: L증가, R증가 방식
  * L, R 초기값: 최소 차이를 찾는 것이기 때문에 최대 차이인 양 끝 포인터보다 L최소 R최소 포인터로 시작하는게 자연스러움
  * while문 범위: 현재 문제 기준, L<N, R<N
+
  */
